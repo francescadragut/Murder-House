f2bde0078f4651371be8ba686602b9f74b0c5e3a
francescadragut <84006907+francescadragut@users.noreply.github.com>
2021-05-31 13:54:10 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -5,9 +5,7 @@
 #ifndef PS_MURDERHOUSE_GAME_H
 #define PS_MURDERHOUSE_GAME_H
 
-
 class Game {
-
     class House
     {
         const int hall;
@@ -19,17 +17,20 @@ class Game {
         const int bathroom;
         const int closet;
         int key;
-    public:
-        House();
-        int getKeyh();
+        
+        public:
+            House();
+            int getKeyh();
     };
+    
     class Killer
     {
         int position;
-    public:
-        Killer();
-        int getPositionk();
-        void setPositionk(int);
+        
+        public:
+            Killer();
+            int getPositionk();
+            void setPositionk(int);
     };
 
     class Player
@@ -37,22 +38,22 @@ class Game {
         char* name;
         int position;
         bool key;
-    public:
-        Player(const char*);
-        void go_to_room(int);
-        const char* getName();
-        void setPosition(int);
-        int getPosition();
-        bool getKey();
-        void setKey(bool);
+        
+        public:
+            Player(const char*);
+            void go_to_room(int);
+            const char* getName();
+            void setPosition(int);
+            int getPosition();
+            bool getKey();
+            void setKey(bool);
     };
 
-public:
-   Game(const char*);
-    House h;
-    Player p;
-    Killer k;
+    public:
+        Game(const char*);
+        House h;
+        Player p;
+        Killer k;
 };
-
 
 #endif //PS_MURDERHOUSE_GAME_H
