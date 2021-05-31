daba7fb89c8afef03ded9f748cd8ad3035ebdafb
francescadragut <84006907+francescadragut@users.noreply.github.com>
2021-05-31 14:07:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,74 +4,73 @@
 
 using namespace std;
 
-int checkWin(int positionp,int positionk,int oldpositionk,bool key){
-    if(positionp==1 && positionk!=positionp && key==true )
+int checkWin(int positionp, int positionk, int oldpositionk, bool key) {
+    if(positionp == 1 && positionk != positionp && key == true)
         return 1;
-    if(positionp==1 && oldpositionk!=positionp && key==true )
+    if(positionp == 1 && oldpositionk != positionp && key == true)
         return 1;
     return 0;
 }
-int checkLose(int positionp,int positionk,int oldpositionk){
-   if(positionp==positionk)
+
+int checkLose(int positionp, int positionk, int oldpositionk) {
+   if(positionp == positionk)
        return 1;
-    if(positionp==oldpositionk)
+   if(positionp == oldpositionk)
         return 1;
    return 0;
 }
 
-
-void peek(int* l,int size, int positionk, int nextpositionk) {
+void peek(int* l, int size, int positionk, int nextpositionk) {
     int ok=0;
-
-    for(int i=0;i<size;i++)
+    for(int i = 0; i < size; i++)
         if (l[i] == positionk || l[i] == nextpositionk) {
             cout << "The killer is in room " << positionk << " and he is going to room " << nextpositionk << endl;
             ok = 1;
         }
-
     if(ok==0)
-        cout<<"The rooms around you are safe!"<<endl;
+        cout << "The rooms around you are safe!" << endl;
 }
-void makePeek(int position, int positionk, int newpositionk){
 
-    if(position==1){
+void makePeek(int position, int positionk, int newpositionk) {
+
+    if(position == 1){
        int a[1] = {2};
-        peek(a,1,positionk,newpositionk);
+       peek(a, 1, positionk, newpositionk);
     }
 
-    if(position==2){
+    if(position == 2){
         int a[5] = {1,3,4,5,7};
-        peek(a,4,positionk,newpositionk);
+        peek(a, 4, positionk, newpositionk);
     }
 
     if(position==3){
         int a[2] = {2,6};
-        peek(a,2,positionk,newpositionk);
+        peek(a, 2, positionk, newpositionk);
     }
 
-    if(position==4){
+    if(position == 4){
         int a[1] = {2};
-        peek(a,1,positionk,newpositionk);
+        peek(a, 1, positionk, newpositionk);
     }
 
-    if(position==5){
+    if(position == 5){
         int a[3] = {2,7,8};
-        peek(a,3,positionk,newpositionk);
+        peek(a, 3, positionk, newpositionk);
     }
 
-    if(position==6){
+    if(position == 6){
         int a[1] = {3};
-        peek(a,1,positionk,newpositionk);
+        peek(a, 1, positionk, newpositionk);
     }
 
-    if(position==7){
+    if(position == 7){
         int a[2] = {2,5};
-        peek(a,1,positionk,newpositionk);
+        peek(a, 1, positionk, newpositionk);
     }
 
-    if(position==8){
+    if(position == 8){
         int a[1] = {5};
-        peek(a,1,positionk,newpositionk);
+        peek(a, 1, positionk, newpositionk);
     }
 
 }
@@ -85,22 +84,21 @@ int main() {
           "You are worried, as your apartment is on the 3rd floor and your only way out would be through the door in the hallway. On the other hand, your apartment has some safety features. Your dressing room is so well hidden that no one who doesn't live in the apartment would ever see it.\n"
           "This is how you comfort yourself, but not for too long... because you start hearing some steps in the hallway.\n\n"
           "Could you be the next victim?\n";
-    cout<<"Player's name:";
+    cout << "Player's name:";
     cin.getline(name,24);
     Game g(name);
     int ch;
-    cout<<g.p.getName()<<", you are in the bedroom. From here, you can (0)peek or you can go to:"<<endl<<"(2)Living Room\n(7)Bathroom\n(8)Dressing Room\n";
+    cout << g.p.getName()<<", you are in the bedroom. From here, you can (0)peek or you can go to:" << endl << "(2)Living Room\n(7)Bathroom\n(8)Dressing Room\n";
 
-    cin>>ch;
-    int path[19]={1,2,3,6,3,2,5,7,5,2,4,2,7,5,2,3,2,5,2};
+    cin >> ch;
+    int path[19] = {1,2,3,6,3,2,5,7,5,2,4,2,7,5,2,3,2,5,2};
     int i = 1;
-    int ok=0;
+    int ok = 0;
     while(checkLose(g.p.getPosition(),g.k.getPositionk(),path[i-2])==0 && checkWin(g.p.getPosition(),g.k.getPositionk(),path[i-2],g.p.getKey())==0)
     {
         if(i==19)
             i=0;
-
-
+        
         switch(ch){
             case 0:
                 makePeek(g.p.getPosition(),g.k.getPositionk(),path[i]);
@@ -120,9 +118,7 @@ int main() {
                     }
                     cout<<"\nYou are in the Hallway. From here,you can (0)peek or you can go to:\n(2)Living Room\n";
                 }
-
                 i++;
-
                 break;
 
             case 2:
@@ -186,6 +182,4 @@ int main() {
     else
         if(checkWin(g.p.getPosition(),g.k.getPositionk(),path[i-2],g.p.getKey())==1)
             cout<<"You survived";
-
-
 }
